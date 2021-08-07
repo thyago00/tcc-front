be19a125cfa9a0cf0859f3eefba69a6b3e2678f2
Lucas <souzamaciel.lucas@gmail.com>
2021-08-07 17:38:41 -0300

    Push projeto

--- App.js
+++ App.js
@@ -8,9 +8,9 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  ); 
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
