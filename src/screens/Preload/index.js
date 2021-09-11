a28ce04e3c0c4179ed5fe3bbbaff3b8ea75d9f32
Lucas Maciel <souzamaciel.lucas@gmail.com>
2021-09-11 19:16:16 -0300

    teste

---
+++ src/screens/Preload/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+import PetLogo from '../../assets/barber.svg'
+export default () => {
+  return (
+    <Container>
+      <PetLogo />
+    </Container>
+  );
+}
