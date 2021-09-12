ff76fd7bb205d1ed644373a2afad81cc27b56948
thyago00 <thyagoro6@gmail.com>
2021-09-11 22:04:03 -0300

    correção

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image, StyleSheet } from 'react-native';
 import { Container } from './styles';
+// import {ReactComponent as PetLogo} from '../../assets/barber.svg'
 import PetLogo from '../../assets/barber.svg'
 export default () => {
   return (
     <Container>
-      <PetLogo />
+      <Image style={styles.img} source={PetLogo}></Image>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  img: {
+    width: 50,
+    height: 200,
+    resizeMode: 'stretch',
+  },
+});
